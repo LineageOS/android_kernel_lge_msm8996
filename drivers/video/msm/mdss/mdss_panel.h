4d02f814c20e40228ee1b06c86cc6de1c955315b
Vinu Deokaran <vinud@codeaurora.org>
2015-11-19 10:36:56 -0500

    msm: mdss: add support to report display id
    Add support to report display ID back to user space through
    panel_info node.
    
    Change-Id: I0d711400b02f441bce17b27bd747034bd5b61b1a
    Signed-off-by: Vinu Deokaran <vinud@codeaurora.org>

--- drivers/video/msm/mdss/mdss_panel.h
+++ drivers/video/msm/mdss/mdss_panel.h
@@ -150,6 +150,7 @@ struct mdss_rect {
 
 #define MDSS_MAX_PANEL_LEN      256
 #define MDSS_INTF_MAX_NAME_LEN 5
+#define MDSS_DISPLAY_ID_MAX_LEN 16
 struct mdss_panel_intf {
 	char name[MDSS_INTF_MAX_NAME_LEN];
 	int  type;
@@ -638,6 +639,7 @@ struct mdss_panel_info {
 
 	bool is_prim_panel;
 	bool is_pluggable;
+	char display_id[MDSS_DISPLAY_ID_MAX_LEN];
 
 	/* refer sim_panel_modes enum for different modes */
 	u8 sim_panel_mode;
