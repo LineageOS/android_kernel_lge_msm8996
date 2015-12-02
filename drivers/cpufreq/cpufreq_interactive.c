d092b2da8fb665a2888740f3355a38b55e9f7fa1
Junjie Wu <junjiew@codeaurora.org>
2015-12-02 10:38:50 -0800

    cpufreq: interactive: Fix potential divide-by-zero operation
    prev_load could be zero if no active time is registered for a CPU
    within a sampling period. Fix potential divide-by-zero issue when
    calculating new load percentage.
    
    Change-Id: I8ad118f5b6b94a410ec59eb5ce939b9467e921c7
    Signed-off-by: Junjie Wu <junjiew@codeaurora.org>
    Signed-off-by: Hanumath Prasad <hpprasad@codeaurora.org>

--- drivers/cpufreq/cpufreq_interactive.c
+++ drivers/cpufreq/cpufreq_interactive.c
@@ -628,7 +628,10 @@ static void cpufreq_interactive_timer(unsigned long data)
 			cputime_speedadj = (u64)sl->prev_load *
 					   ppol->policy->cpuinfo.max_freq;
 			do_div(cputime_speedadj, tunables->timer_rate);
-			new_load_pct = sl->new_task_load * 100 / sl->prev_load;
+			new_load_pct = 0;
+			if (sl->prev_load)
+				new_load_pct = sl->new_task_load * 100 /
+						sl->prev_load;
 		} else {
 			now = update_load(i);
 			delta_time = (unsigned int)
