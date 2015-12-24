3b2eb105f6802378c7603fe7bf39c2f575ce97f1
Sandeep Panda <spanda@codeaurora.org>
2015-12-24 08:22:48 +0530

    msm: mdss: read DSI CTRL and PHY revision properly
    In the current implementation DSI PHY revision is read once
    DSI PHY initialization is complete. But this needs to be done
    before initializing PHY, so that correct PHY initialization
    sequence can be executed.
    
    Change-Id: I6f0cd59d63d0a8c0a5d47284f2e86e72f37d099f
    Signed-off-by: Sandeep Panda <spanda@codeaurora.org>

--- drivers/video/msm/mdss/msm_mdss_io_8974.c
+++ drivers/video/msm/mdss/msm_mdss_io_8974.c
@@ -1944,9 +1944,12 @@ int mdss_dsi_post_clkon_cb(void *priv,
 	pdata = &ctrl->panel_data;
 
 	if (clk & MDSS_DSI_CORE_CLK) {
-		if (!pdata->panel_info.cont_splash_enabled)
+		if (!pdata->panel_info.cont_splash_enabled) {
 			mdss_dsi_read_hw_revision(ctrl);
+			mdss_dsi_read_phy_revision(ctrl);
+		}
 		MDSS_XLOG(ctrl->ndx, MIPI_INP(ctrl->phy_io.base + 0x10));
+
 		/*
 		 * Phy and controller setup is needed if coming out of idle
 		 * power collapse with clamps enabled.
