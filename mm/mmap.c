c79f08a8a30437b14a2d6cdf06f6807f200a7da9
Rashed Abdel-Tawab <rashed@linux.com>
2016-07-14 01:04:15 -0400

    Revert "Build Fixes (per Rashed)"
    This reverts commit 6ebd6b5f95f5104a83db919b4cbbbefe3b76c68c.

--- mm/mmap.c
+++ mm/mmap.c
@@ -1265,12 +1265,9 @@ static inline int mlock_future_check(struct mm_struct *mm,
  * The caller must hold down_write(&current->mm->mmap_sem).
  */
 
-
-#if 0
 extern void set_app_setting_bit(uint32_t bit);
 extern char *lib_names[];
-extern unsigned int count;
-#endif
+extern unsigned int lib_name_entries;
 
 unsigned long do_mmap_pgoff(struct file *file, unsigned long addr,
 			unsigned long len, unsigned long prot,
@@ -1281,13 +1278,12 @@ unsigned long do_mmap_pgoff(struct file *file, unsigned long addr,
 	vm_flags_t vm_flags;
 
 	*populate = 0;
-#if 0
 	if (file && file->f_path.dentry) {
 		const char *name = file->f_path.dentry->d_name.name;
 		bool found = false;
 		int i;
 
-		for (i = 0; i < count; i++) {
+		for (i = 0; i < lib_name_entries; i++) {
 			if (unlikely(!strcmp(name, lib_names[i]))) {
 				found = true;
 				break;
@@ -1303,7 +1299,6 @@ unsigned long do_mmap_pgoff(struct file *file, unsigned long addr,
 			preempt_enable();
 		}
 	}
-#endif
 
 #ifdef CONFIG_SDCARD_FS_ANDROID_M
 	while (file && (file->f_mode & FMODE_NOMAPPABLE))
