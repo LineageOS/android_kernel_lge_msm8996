577080c3d6943edbcd8adae602f5605b93086a47
Eric Biggers <ebiggers@google.com>
2017-04-12 11:09:23 -0700

    ANDROID: pfk: allow encryption key to be specified in bio
    Introduce the ability for an encryption key to be passed through 'struct
    bio' to the "PFK" module for use by inline encryption with Qualcomm ICE.
    This will be used to specify a default key for metadata encryption.
    
    Instead of referencing the keyring key (struct key) directly as we did
    in the marlin kernel, require users to provide a structure directly
    containing the key material.  Referencing a keyring key from struct bio
    is incorrect because userspace can call keyctl_revoke() at any time,
    causing the key's payload to be freed; so unless the key semaphore is
    held, there's no guarantee the payload will live until the bio is done.
    
    For now, leave in place most of the PFK code, including the "get the
    bio's inode and call back into ext4 to get the key" hack.  This is
    necessary for now, though really this should be implemented by having
    ext4 set the key in its bios, and refactoring the "PFK" module's
    functionality into more appropriate places in block/ and drivers/.  But
    in any case, keys specified by ext4 will override the "default" key, as
    intended.
    
    Signed-off-by: Eric Biggers <ebiggers@google.com>
    Change-Id: I4295abaeecf6f7ab8c9854eb80d928f40500fde2

--- block/bio.c
+++ block/bio.c
@@ -544,6 +544,13 @@ inline int bio_phys_segments(struct request_queue *q, struct bio *bio)
 }
 EXPORT_SYMBOL(bio_phys_segments);
 
+static inline void bio_clone_crypt_key(struct bio *dst, const struct bio *src)
+{
+#ifdef CONFIG_PFK
+	dst->bi_crypt_key = src->bi_crypt_key;
+#endif
+}
+
 /**
  * 	__bio_clone_fast - clone a bio that shares the original bio's biovec
  * 	@bio: destination bio
@@ -569,6 +576,8 @@ void __bio_clone_fast(struct bio *bio, struct bio *bio_src)
 	bio->bi_iter = bio_src->bi_iter;
 	bio->bi_io_vec = bio_src->bi_io_vec;
 	bio->bi_dio_inode = bio_src->bi_dio_inode;
+
+	bio_clone_crypt_key(bio, bio_src);
 }
 EXPORT_SYMBOL(__bio_clone_fast);
 
@@ -673,6 +682,8 @@ integrity_clone:
 			return NULL;
 		}
 	}
+
+	bio_clone_crypt_key(bio, bio_src);
 
 	return bio;
 }
