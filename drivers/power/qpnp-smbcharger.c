e1e29ed881e3eabd8f5bf453b650a84a21621e04
Diogo Ferreira <diogo@underdev.org>
2017-05-06 17:14:00 +0100

    msm8996: qpnp-smbcharger: Publish charger voltage to the usb psy
    Since N, the Keyguard uses watt data rather than amperage alone. It
    needs both voltage and amperage information for the power supply
    subsystem to do so.
    
    This brings back the "Charging slowly", "Charging" and "Charging
    rapidly" texts on the lock screen.
    
    Change-Id: Iac37d8b69d2acc048e8c132b2c26c2bed3676d75
    Signed-off-by: Tab Fitts <tfitts1212@gmail.com>

--- drivers/power/qpnp-smbcharger.c
+++ drivers/power/qpnp-smbcharger.c
@@ -2932,6 +2932,8 @@ static void smbchg_parallel_usb_enable(struct smbchg_chip *chip,
 	pr_smb(PR_LGE, "New main ICL percent = %d\n", smbchg_main_chg_icl_percent);
 #endif
 
+	power_supply_set_voltage_limit(chip->usb_psy,
+			(chip->vfloat_mv + 50) * 1000);
 	/* Set USB ICL */
 	target_icl_ma = get_effective_result_locked(chip->usb_icl_votable);
 	new_parallel_cl_ma = total_current_ma
@@ -3943,6 +3945,8 @@ static int smbchg_float_voltage_set(struct smbchg_chip *chip, int vfloat_mv)
 #ifdef CONFIG_LGE_PM_CYCLE_BASED_CHG_VOLTAGE
         batt_life_cycle_set_fcc_ma(chip);
 #endif
+		power_supply_set_voltage_limit(chip->usb_psy,
+				chip->vfloat_mv * 1000);
 	}
 
 	return rc;
