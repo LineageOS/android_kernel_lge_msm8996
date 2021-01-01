bd1847fabc5a52874f71106030a25c47968ac6dd
darkalexpp <darkalexpp@gmail.com>
2020-12-31 20:59:03 -0500

    Kernel: Update linux kernel to 3.18.125

--- build.sh
+++ build.sh
@@ -5,14 +5,14 @@ RDIR=$(pwd)
 # build dir
 BDIR=build
 
-ZBIR=out
+ZDIR=out
 
 export ARCH=arm64
 export CROSS_COMPILE=$HOME/build/toolchain/lineageos/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9/bin/aarch64-linux-android-
 export CROSS_COMPILE_ARM32=$HOME/build/toolchain/lineageos/android_prebuilts_gcc_linux-x86_arm_arm-linux-androideabi-4.9/bin/arm-linux-androideabi-
 ##############G5############################
 mkdir -p "$BDIR" 
-mkdir -p out
+mkdir -p "$ZDIR"
 ##############G5############################
 
 make -C "$RDIR" O="$BDIR"  lineageos_rs988_defconfig
--- drivers/usb/core/driver.c
+++ drivers/usb/core/driver.c
@@ -541,9 +541,20 @@ int usb_driver_claim_interface(struct usb_driver *driver,
 	if (device_is_registered(dev))
 		retval = device_bind_driver(dev);
 
-	/* Attempt to re-enable USB3 LPM, if the disable was successful. */
-	if (!lpm_disable_error)
-		usb_unlocked_enable_lpm(udev);
+	if (retval) {
+		dev->driver = NULL;
+		usb_set_intfdata(iface, NULL);
+		iface->needs_remote_wakeup = 0;
+		iface->condition = USB_INTERFACE_UNBOUND;
+
+		/*
+		 * Unbound interfaces are always runtime-PM-disabled
+		 * and runtime-PM-suspended
+		 */
+		if (driver->supports_autosuspend)
+			pm_runtime_disable(dev);
+		pm_runtime_set_suspended(dev);
+	}
 
 	return retval;
 }
